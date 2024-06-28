5f97c47079fbf158544ff8a3086c5c0bee308fbc
sunghyuklee <merci726@yahoo.com>
2024-06-28 21:17:56 +0900

    update about fixed some

--- src/main/java/org/proleesh/gabbi/controller/ChatController.java
+++ src/main/java/org/proleesh/gabbi/controller/ChatController.java
@@ -8,14 +8,16 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ChatController {
-
     @GetMapping("/chat")
-    public String chat(){
+    public String chatStart() {
         return "chat/chat";
     }
+
 
 
     @MessageMapping("/chat.sendMessage")
